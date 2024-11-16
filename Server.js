cf2e0b0b0f5f236d7dc12392ba8c34931ce96e79
AdrianOjeda <adrian.16300022@gmail.com>
2024-11-15 21:10:56 -0600

    Test commit

--- Server.js
+++ Server.js
@@ -50,7 +50,7 @@ app.get('/users/:email', async (req, res) => {
         return res.status(500).json({ message: 'Error al obtener datos de Supabase', error: error.message });
     }
 });
-
+   
 const bcrypt = require('bcrypt');
 
 app.post('/register', async (req, res) => {
